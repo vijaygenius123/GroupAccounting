2c30283e0f917683998695c582538f752308b431
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-09-15 06:44:40 +0100

    Initial Commit

---
+++ GroupAccounting/GroupAccountingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupAccountingApp.swift
+//  GroupAccounting
+//
+//  Created by Vijayaraghavan Sundararaman on 15/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct GroupAccountingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
